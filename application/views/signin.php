e8497e6fae5f5ee65081dd4bd32f6f86285a902b
vila buleje jhonatan andree <jhonatan20092009@gmail.com>
2021-10-10 17:58:14 -0500

    cambio en favicon sing

--- application/views/signin.php
+++ application/views/signin.php
@@ -19,7 +19,7 @@
   <meta name="author" content="Creative Tim">
   <title>Nayeli</title>
   <!-- Favicon -->
-  <link rel="icon" href="<?php echo base_url(); ?>assets/img/brand/nayeli.png" type="image/png">
+  <link rel="icon" href="<?php echo base_url(); ?>assets/img/nayeli.png" type="image/png">
   <!-- Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
   <!-- Icons -->
